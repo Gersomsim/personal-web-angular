054abfa3c1e3242b3425ced13bd63a36efe4a0e7
Gersomsim <gesomhernandez@me.com>
2026-02-20 13:35:56 -0600

    ⚡️ Autorizacion Token en post

--- src/app/features/posts/components/post-form/post-form.ts
+++ src/app/features/posts/components/post-form/post-form.ts
@@ -1,15 +1,17 @@
-import { Component, DestroyRef, OnInit, inject, input, output, signal } from '@angular/core'
+import { Component, DestroyRef, OnInit, computed, effect, inject, input, output, signal } from '@angular/core'
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 
 import { slugify } from '@core/utils'
+import { Category } from '@features/categories/models'
 import { PostFormData } from '@features/posts/dto'
 import { Post } from '@features/posts/models'
-import { Editor, FormInput, FormSelect, FormTagsPicker, FormTextarea, FormToggle } from '@shared/components'
+import { Tag } from '@features/tags/models'
+import { Combobox, ComboboxItem, ComboboxMulti, Editor, FormInput, FormTextarea, FormToggle } from '@shared/components'
 
 @Component({
 	selector: 'app-post-form',
-	imports: [ReactiveFormsModule, Editor, FormInput, FormTextarea, FormSelect, FormToggle, FormTagsPicker],
+	imports: [ReactiveFormsModule, Editor, FormInput, FormTextarea, FormToggle, ComboboxMulti, Combobox],
 	templateUrl: './post-form.html',
 	styles: ``,
 })
@@ -18,8 +20,31 @@ export class PostForm implements OnInit {
 	private fb = inject(FormBuilder)
 
 	post = input<Post>()
+	categories = input<Category[]>()
+	categoryCreated = input<Category | null>(null)
+	reset = input<boolean>(false)
+
+	tags = input<Tag[]>()
+	tagCreated = input<Tag>()
 	loading = input<boolean>(false)
+
 	formSubmit = output<PostFormData>()
+	createCategoryRequested = output<string>()
+	createTagRequested = output<string>()
+
+	categoryOptions = computed<ComboboxItem[]>(() => this.categories()?.map(c => ({ value: c.name, key: c.id })) ?? [])
+	tagOptions = computed<ComboboxItem[]>(() => this.tags()?.map(t => ({ key: t.id, value: t.name })) ?? [])
+	readonly errorCategory = computed(() => {
+		const errors = this.f.categoryId!.errors // Suponiendo que pasas el control como input
+		if (!errors) return null
+
+		const errorKeys = Object.keys(errors)
+		const messages: Record<string, string> = {
+			required: 'Este campo es obligatorio.',
+		}
+
+		return messages[errorKeys[0]] || 'Campo inválido'
+	})
 
 	private slugEdited = signal(false)
 
@@ -28,15 +53,33 @@ export class PostForm implements OnInit {
 		slug: ['', [Validators.required]],
 		excerpt: ['', [Validators.required, Validators.minLength(50)]],
 		descriptionSeo: ['', [Validators.required, Validators.minLength(50)]],
-		keywordsSeo: ['', [Validators.required, Validators.minLength(50)]],
-		content: ['', [Validators.required, Validators.minLength(50)]],
-		image: ['', [Validators.required, Validators.minLength(50)]],
-		categoryId: ['', [Validators.required, Validators.minLength(50)]],
-		tagsId: ['', [Validators.required, Validators.minLength(50)]],
-		date: ['', [Validators.required]],
+		keywordsSeo: ['', [Validators.required, Validators.minLength(10)]],
+		content: ['', [Validators.required]],
+		image: [''],
+		categoryId: ['', [Validators.required]],
+		tagsId: [[] as string[], [Validators.required]],
 		readTime: ['', [Validators.required]],
 		featured: [false],
 	})
+
+	constructor() {
+		effect(() => {
+			if (this.categoryCreated()) {
+				this.form.patchValue({ categoryId: this.categoryCreated()!.id })
+			}
+		})
+		effect(() => {
+			if (this.reset()) {
+				this.form.reset()
+			}
+		})
+		effect(() => {
+			if (this.tagCreated()) {
+				const tagsId = this.form.value.tagsId ?? []
+				this.form.patchValue({ tagsId: [...tagsId, this.tagCreated()!.id] })
+			}
+		})
+	}
 
 	get f() {
 		return this.form.controls
@@ -76,8 +119,19 @@ export class PostForm implements OnInit {
 		const v = this.form.getRawValue()
 		const data: PostFormData = {
 			...v,
-			tagsId: v.tagsId.split(',').map(tag => tag.trim()),
 		}
 		this.formSubmit.emit(data)
+	}
+	createCategory(name: string) {
+		this.createCategoryRequested.emit(name)
+	}
+	loadCategory(id: string | number) {
+		this.form.patchValue({ categoryId: id.toString() })
+	}
+	createTag(name: string) {
+		this.createTagRequested.emit(name)
+	}
+	loadTag(ids: (string | number)[]) {
+		this.form.patchValue({ tagsId: ids.map(id => id.toString()) })
 	}
 }
--- src/app/features/posts/services/post.service.ts
+++ src/app/features/posts/services/post.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core'
+import { Injectable, inject } from '@angular/core'
 
 import { environment } from '@env/environment'
+import { AuthFacade } from '@features/auth/facade'
 import { ApiResponse } from '@shared/dto/api-response.dto'
 import { ListResponse } from '@shared/dto/list-response.dto'
 
+import { PostFormData } from '../dto'
 import { QueryPostDto } from '../dto/query-post.dto'
 import { Post } from '../models'
 
@@ -11,6 +13,7 @@ import { Post } from '../models'
 	providedIn: 'root',
 })
 export class PostService {
+	private readonly authFacade = inject(AuthFacade)
 	private readonly apiUrl = `${environment.apiBaseUrl}/posts`
 
 	async getAll(filters?: QueryPostDto): Promise<ListResponse<Post>> {
@@ -33,6 +36,44 @@ export class PostService {
 	async getBySlug(slug: string): Promise<Post> {
 		await new Promise(resolve => setTimeout(resolve, 2000))
 		const response = await fetch(`${this.apiUrl}/${slug}`)
+		const res: ApiResponse<Post> = await response.json()
+		return res.data
+	}
+
+	async create(payload: PostFormData): Promise<Post> {
+		const response = await fetch(`${this.apiUrl}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${this.authFacade.token()}`,
+			},
+			body: JSON.stringify(payload),
+		})
+		const res: ApiResponse<Post> = await response.json()
+		return res.data
+	}
+
+	async update(id: string, payload: PostFormData): Promise<Post> {
+		const response = await fetch(`${this.apiUrl}/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${this.authFacade.token()}`,
+			},
+			body: JSON.stringify(payload),
+		})
+		const res: ApiResponse<Post> = await response.json()
+		return res.data
+	}
+
+	async delete(id: string): Promise<Post> {
+		const response = await fetch(`${this.apiUrl}/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${this.authFacade.token()}`,
+			},
+		})
 		const res: ApiResponse<Post> = await response.json()
 		return res.data
 	}
