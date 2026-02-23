38707c49f6d1f8d8521d390fa73b323c33b58f59
Gersomsim <gesomhernandez@me.com>
2026-02-23 14:52:04 -0600

    Actualizacion de vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-const app = await import('../dist/portfolio/server/server.mjs')
-
-export default app
+export default async (req, res) => {
+	const { reqHandler } = await import('../dist/portfolio/server/server.mjs')
+	return reqHandler(req, res)
+}
