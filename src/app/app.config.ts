265a599fa7104ee8a085e7538b0b42ddfc612455
Gersomsim <gesomhernandez@me.com>
2026-02-22 18:45:08 -0600

    SSR habilitado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
 import { provideRouter, withInMemoryScrolling } from '@angular/router'
 
-import { routes } from './app.routes'
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -9,6 +10,6 @@ export const appConfig: ApplicationConfig = {
 		provideRouter(
 			routes,
 			withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' }),
-		),
+		), provideClientHydration(withEventReplay()),
 	],
 }
