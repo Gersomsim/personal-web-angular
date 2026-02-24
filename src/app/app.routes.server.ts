865821b8ca1c63e98105cdff03b070c29d57c91e
Gersomsim <gesomhernandez@me.com>
2026-02-24 13:56:11 -0600

    Prerender de proyectos capturados

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr'
 
 export const serverRoutes: ServerRoute[] = [
 	{
+		path: 'portfolio/:slug',
+		renderMode: RenderMode.Prerender,
+		getPrerenderParams: async () => {
+			return [
+				{ slug: 'erp-aquos-gestion-de-suscripciones-y-activos' },
+				{ slug: 'clientes-aquos' },
+				{ slug: 'sistema-sorfi' },
+				{ slug: 'crm-aclara-automatizacion-comercial' },
+				{ slug: 'gestion-integral-empresarial-erp' },
+			]
+		},
+	},
+	// 2. Todo lo demás será SSR puro
+	{
 		path: '**',
 		renderMode: RenderMode.Server,
 	},
